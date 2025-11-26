d3caed83eac5179bff44028ccfc72a8940aa7017
AzamatMedetbekov <azamatmedetbekovv@gmail.com>
2025-11-27 00:48:29 +0900

    docs: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('hurry-curry-be')
+    .setDescription('hurry-curry-be')
+    .setVersion('1.0')
+    .addTag('Fall longckathon')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log('http://localhost:3000/api');
 }
 bootstrap();
