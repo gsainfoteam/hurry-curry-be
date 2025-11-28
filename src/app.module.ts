93edebe484547f5c8829218049fce2d07fe5f200
AzamatMedetbekov <azamatmedetbekovv@gmail.com>
2025-11-28 15:06:51 +0900

    bullModule setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), OrdersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    OrdersModule,
+    AuthModule,
+
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get<number>('REDIS_PORT'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+
+    BullModule.registerQueue({
+      name: 'curry-queue',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
