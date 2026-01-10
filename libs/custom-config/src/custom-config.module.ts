8d262d96f8f92a9797a68f90ba704359deb24971
AzamatMedetbekov <azamatmedetbekovv@gmail.com>
2026-01-10 22:01:46 +0900

    chore: monolith to monorepo structure

---
+++ libs/custom-config/src/custom-config.module.ts
@@ -0,0 +1,4 @@
+import { Module } from '@nestjs/common';
+
+@Module({ providers: [], exports: [], imports: [] })
+export class CustomConfigModule {}
---
+++ libs/prisma/src/prisma.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { CustomConfigModule } from 'libs/custom-config/src/custom-config.module';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+  imports: [CustomConfigModule],
+})
+export class PrismaModule {}
